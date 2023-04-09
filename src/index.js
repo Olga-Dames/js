9bea0c0bcf7a24fca2cb6f323e8e3e790a0ab702
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-04-09 10:39:12 +0300

    1

--- src/index.js
+++ src/index.js
@@ -20,12 +20,14 @@ async function onLoad(e) {
         return acc;
       }, {});
       window.localStorage.setItem('genres', JSON.stringify(genresToSave));
+    } catch (error) {}
+  }
+      try {
       const { results } = await API.getPopularMovies();
       insertCardMarkup(results);
     } catch (error) {
       console.log(error);
     }
-  }
 }
 
 const getGenresNames = ids => {
@@ -39,12 +41,23 @@ const getGenresNames = ids => {
 const insertCardMarkup = movies => {
   const cardMarkup = movies
     .map(({ title, release_date, poster_path, genre_ids, first_air_date }) => {
-      const getGenreNames = getGenresNames(genre_ids);   
+      const getGenreNames = getGenresNames(genre_ids);
+
+      const movieData = {
+        release_date,
+        first_air_date,
+      };
+      let releaseDate;
+      if (movieData.release_date) {
+        releaseDate = movieData.release_date;
+      } else if (movieData.first_air_date) {
+        releaseDate = movieData.first_air_date;
+      }
       return `
 <li class=film_card>
 <img class=film_poster src=https://image.tmdb.org/t/p/original${poster_path} width= 50 height= 50 alt= ${title}/>
 <p class=film_name>${title}</p>
-<p class=movie-gener>${getGenreNames}${release_date}</p>
+<p class=movie-gener>${getGenreNames}${releaseDate.slice(0,4)}</p>
 </li>`;
     })
     .join('');
